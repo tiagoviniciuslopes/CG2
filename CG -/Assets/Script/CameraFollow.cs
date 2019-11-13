4b9631d24d4176514ee74c431e82bf18c980db0a
tiagoviniciuslopes <tiago.foz@hotmail.com>
2019-11-13 20:48:07 -0300

    mudando camera

---
+++ CG -/Assets/Script/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+    float speed = 0.125f;
+    [SerializeField]
+    Vector3 offset;
+
+    void LateUpdate()
+    {
+        transform.position = player.position + offset;
+    }
+}
